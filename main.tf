22f1dfb51496bbed3c5dedc421c8a4310bad7b03
Ernsnare <78518512+Ernsnare@users.noreply.github.com>
2023-07-19 17:24:35 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "generic" {
+  ip_address = "172.17.32.96"  # Ganti dengan alamat IP server on-premise Anda
+  ssh_key_file = "~/.ssh/id_rsa"  # Ganti dengan path ke kunci SSH Anda
+  ssh_username = "root"  # Ganti dengan username SSH Anda
+  ssh_password = "seninselasa73" # Password ssh
+}
